32e027f057f76b2d2b4d87161d306cb8454ab3bf
Rahul Kumar Yadav <ry642499@gmail.com>
2026-02-25 13:47:40 +0530

    Ignore spring_gmail_seevice module

--- spring_gmail_seevice/src/main/java/com/gmail/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface EmailService {
-    void sendEmail(String to, String name, MultipartFile file);
-}
